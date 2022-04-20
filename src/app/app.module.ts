54bafea53d4e583f21861ac04459620e14182f5e
Marcosjr <mjworks2020@outlook.com>
2022-04-20 17:48:48 -0300

    Added new components for login screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { ResetPasswordPageComponent } from './components/reset-password-page/reset-password-page.component';
+import { SignupPageComponent } from './components/signup-page/signup-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginPageComponent,
+    ResetPasswordPageComponent,
+    SignupPageComponent
   ],
   imports: [
     BrowserModule,
