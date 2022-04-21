1960260ac5c45d1317e74c1155a95f3dfc14adf1
Marcosjr <mjworks2020@outlook.com>
2022-04-21 12:05:20 -0300

    Organized and removed desnecessary archives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { LoginPageComponent } from './components/login-page/login-page.component';
-import { ResetPasswordPageComponent } from './components/reset-password-page/reset-password-page.component';
-import { SignupPageComponent } from './components/signup-page/signup-page.component';
-import { PetsPageComponent } from './components/pets-page/pets-page.component';
-import { ProductsPageComponent } from './components/products-page/products-page.component';
-import { CartPageComponent } from './components/cart-page/cart-page.component';
+import { LoginPageComponent } from './pages/account/login-page/login-page.component';
+import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
+import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
+import { PetsPageComponent } from './pages/account/pets-page/pets-page.component';
+import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
+import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
 
 @NgModule({
   declarations: [
@@ -20,13 +20,10 @@ import { CartPageComponent } from './components/cart-page/cart-page.component';
     SignupPageComponent,
     PetsPageComponent,
     ProductsPageComponent,
-    CartPageComponent
+    CartPageComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
