053b1478cdeea91179475ace4c548206eb3deac5
MJ DEV <marcosjniorpassarellanaves@pop-os.localdomain>
2022-04-30 19:34:41 -0300

    Added model and more updates

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -5,11 +5,11 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class DataService {
-  private url: string = 'http://localhost:3000/';
+  private url: string = 'http://localhost:3000/v1';
 
   constructor(private http: HttpClient) {}
 
   getProducts() {
-    return this.http.get<any[]>(`${this.url}products`);
+    return this.http.get<any[]>(`${this.url}/products`);
   }
 }
