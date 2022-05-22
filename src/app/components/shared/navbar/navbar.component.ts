d36eec9a1d422683e33536c4e8be362d4cbe7761
MJWORKS <mjworks2020@outlook.com>
2022-05-22 12:34:53 -0300

    Added router guard

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../../models/user.model';
+import { Security } from '../../../utils/secury.util';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent implements OnInit {
-  constructor() {}
+  public user!: User;
 
-  ngOnInit(): void {}
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.user = Security.getUser();
+  }
+
+  logout(): void {
+    Security.clear();
+    this.router.navigate(['/login']);
+  }
 }
--- src/app/utils/secury.util.ts
+++ src/app/utils/secury.util.ts
@@ -16,20 +16,22 @@ export class Security {
     localStorage.setItem('petshoptoken', token);
   }
 
-  public static getUser(): User | null  {
+  public static getUser(): User {
     const data = localStorage.getItem('petshopuser');
     if (data) {
       return JSON.parse(atob(data));
     } else {
+      //@ts-ignore
       return null;
     }
   }
 
-  public static getToken(): string | null {
+  public static getToken(): string {
     const data = localStorage.getItem('petshoptoken');
     if (data) {
       return data;
     } else {
+      //@ts-ignore
       return null;
     }
   }
